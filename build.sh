80b0c64bbd091f0ba6811ca9b55c31b7f4961e42
axm6392 <axm6392@rit.edu>
2020-02-21 03:14:26 -0500

    Updated directory followed the gradle default path and made a new file build.gradle

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d build src/java/*.java
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build -d build test/java/*.java
 
-java -cp build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build/.:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
 java -cp build RunEdgeConvert
